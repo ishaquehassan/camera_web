0673f92d1e180bddc85acf2594a7e6658d490ea5
ishaqhassan <ishaquehassan@gmail.com>
2025-03-21 09:07:11 +0500

    Adjusted ResolutionPreset.veryHigh to 2160x4096.

--- lib/src/camera_service.dart
+++ lib/src/camera_service.dart
@@ -266,7 +266,7 @@ class CameraService {
       case ResolutionPreset.ultraHigh:
         return const Size(4096, 2160);
       case ResolutionPreset.veryHigh:
-        return const Size(720,1280);
+        return const Size(2160,4096);
       case ResolutionPreset.high:
         return const Size(1280, 720);
       case ResolutionPreset.medium:
